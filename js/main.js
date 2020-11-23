6ddf4f5f1dda0ead90eaccda866cb5262680b506
yuri240190 <killer240190@mail.ru>
2020-11-23 21:02:16 +0300

    create:swiper-slider  in otel section

---
+++ js/main.js
@@ -0,0 +1,14 @@
+var mySwiper = new Swiper('.swiper-container', {
+  // Optional parameters
+  loop: true,
+  // Navigation arrows
+  navigation: {
+    nextEl: '.slider-button--next',
+    prevEl: '.slider-button--prev',
+  },
+  effect:'coverflow',
+ keyboard: {
+    enabled: true,
+    onlyInViewport: false,
+  },
+})
