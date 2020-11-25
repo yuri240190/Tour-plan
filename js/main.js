1371b497ab7806fa2a1563f9d71d83d0d95a0c74
yuri240190 <killer240190@mail.ru>
2020-11-25 15:24:04 +0300

    create:api yandex

--- js/main.js
+++ js/main.js
@@ -12,3 +12,27 @@ var mySwiper = new Swiper('.swiper-container', {
     onlyInViewport: false,
   },
 })
+var myMap;
+
+// Дождёмся загрузки API и готовности DOM.
+ymaps.ready(init);
+
+function init () {
+    // Создание экземпляра карты и его привязка к контейнеру с
+    // заданным id ("map").
+    myMap = new ymaps.Map('map', {
+        // При инициализации карты обязательно нужно указать
+        // её центр и коэффициент масштабирования.
+        center: [7.839145110780258, 98.29909090764404], //Пхукет
+        zoom: 15
+
+    }, {
+        searchControlProvider: 'yandex#search'
+    });
+
+    document.getElementById('destroyButton').onclick = function () {
+        // Для уничтожения используется метод destroy.
+        myMap.destroy();
+    };
+
+}
