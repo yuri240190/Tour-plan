c81652c58d8f57e2b77c9e9a6c93724048c88e11
yuri240190 <killer240190@mail.ru>
2020-12-05 13:53:46 +0300

    create

--- js/main.js
+++ js/main.js
@@ -1,91 +1,94 @@
-$(document).ready(function() {
-var hotelSlider = new Swiper('.hotel-slider', {
-  // Optional parameters
-  loop: true,
-  // Navigation arrows
-  navigation: {
-    nextEl: '.hotel-slider__button--next',
-    prevEl: '.hotel-slider__button--prev',
-  },
-  effect:'coverflow',
- keyboard: {
-    enabled: true,
-    onlyInViewport: false,
-  },
-})
-var reviewsSlider = new Swiper('.reviews-slider', {
-  // Optional parameters
-  loop: true,
-  // Navigation arrows
-  navigation: {
-    nextEl: '.reviews-slider__button--next',
-    prevEl: '.reviews-slider__button--prev',
-  },
-  
- keyboard: {
-    enabled: true,
-    onlyInViewport: false,
-  },
-})
-var myMap;
+$(document).ready(function () {
+  var hotelSlider = new Swiper('.hotel-slider', {
+    // Optional parameters
+    loop: true,
+    // Navigation arrows
+    navigation: {
+      nextEl: '.hotel-slider__button--next',
+      prevEl: '.hotel-slider__button--prev',
+    },
+    effect: 'coverflow',
+    keyboard: {
+      enabled: true,
+      onlyInViewport: false,
+    },
+  })
+  var reviewsSlider = new Swiper('.reviews-slider', {
+    // Optional parameters
+    loop: true,
+    // Navigation arrows
+    navigation: {
+      nextEl: '.reviews-slider__button--next',
+      prevEl: '.reviews-slider__button--prev',
+    },
 
-// Дождёмся загрузки API и готовности DOM.
-ymaps.ready(init);
+    keyboard: {
+      enabled: true,
+      onlyInViewport: false,
+    },
+  })
+  var myMap;
 
-function init () {
+  // Дождёмся загрузки API и готовности DOM.
+  ymaps.ready(init);
+
+  function init() {
     // Создание экземпляра карты и его привязка к контейнеру с
     // заданным id ("map").
     myMap = new ymaps.Map('map', {
-        // При инициализации карты обязательно нужно указать
-        // её центр и коэффициент масштабирования.
-        center: [7.839145110780258, 98.29909090764404], //Пхукет
-        zoom: 15
+      // При инициализации карты обязательно нужно указать
+      // её центр и коэффициент масштабирования.
+      center: [7.839145110780258, 98.29909090764404], //Пхукет
+      zoom: 15
 
     }, {
-        searchControlProvider: 'yandex#search'
+      searchControlProvider: 'yandex#search'
     });
 
     document.getElementById('destroyButton').onclick = function () {
-        // Для уничтожения используется метод destroy.
-        myMap.destroy();
+      // Для уничтожения используется метод destroy.
+      myMap.destroy();
     };
 
-}
-var menuButton = document.querySelector('.menu-button');
-menuButton.addEventListener('click' , function (){
-  console.log('клик по кнопки меню')
-  document
-  .querySelector(".navbar-bottom")
-  .classList.toggle("navbar-bottom-visible");
- document.getElementsByTagName("body")[0].classList.toggle("fixed");
+  }
+  var menuButton = document.querySelector('.menu-button');
+  menuButton.addEventListener('click', function () {
+    console.log('клик по кнопки меню')
+    document
+      .querySelector(".navbar-bottom")
+      .classList.toggle("navbar-bottom-visible");
+    document.getElementsByTagName("body")[0].classList.toggle("fixed");
+
+
+  });
+  
+   var bookingButton = document.querySelector('.booking__button');
+  bookingButton.addEventListener('click', function () {
+  document.getElementsByTagName("body")[0].classList.toggle("body-fixed");
+
+  });
+ 
+ 
+
+
+  var modalButton = $("[data-toggle=modal]");
+  var closeModalButton = $(".modal__close");
+  modalButton.on("click", openModal);
+  closeModalButton.on("click", closeModal);
+
+  function openModal() {
+    var modalOverlay = $(".modal__overlay");
+    var modalDialog = $(".modal__dialog");
+    modalOverlay.addClass("modal__overlay--visible")
+    modalDialog.addClass("modal__dialog--visible")
+
+  }
+  function closeModal() {
+    var modalOverlay = $(".modal__overlay");
+    var modalDialog = $(".modal__dialog");
+    modalOverlay.removeClass("modal__overlay--visible")
+    modalDialog.removeClass("modal__dialog--visible")
+
+  };
 });
 
-var modalButton = $("[data-toggle=modal]");
-var closeModalButton = $(".modal__close");
-modalButton.on("click", openModal);
-closeModalButton.on("click", closeModal);
-
-function openModal(){
-  var modalOverlay = $(".modal__overlay");
-  var modalDialog = $(".modal__dialog");
-  modalOverlay.addClass("modal__overlay--visible")
-   modalDialog.addClass("modal__dialog--visible")
-
-}
-function closeModal(){
-  var modalOverlay = $(".modal__overlay");
-  var modalDialog = $(".modal__dialog");
-  modalOverlay.removeClass("modal__overlay--visible")
-   modalDialog.removeClass("modal__dialog--visible")
-
-}
-document.body.addEventListener('keyup', function closeModal() {
-    var key = e.keyCode;
-
-    if (key == 27) {
-        document.querySelector('modal__overlay--visible').classList.remove('modal__overlay--visible');
-        document.querySelector('modal__dialog--visible').classList.remove('modal__dialog--visible');
-    };
-});
-
-});
